79f3dea64107c27a517a87d74edd99b19ba76be6
emotionalproof <chaserabenn@gmail.com>
2020-07-20 02:00:39 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,33 @@
-// create your App component here
+import React, { Component } from 'react'
+
+class App extends Component {
+    state = {
+        astros: []
+    }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+        .then(resp => resp.json())
+        .then(data => {
+            let people = data.people
+            this.setState({
+                astros: people
+            })
+            })
+    }
+
+    render() {
+        console.log(this.state.astros)
+        const astronauts = this.state.astros.map((astro, index) =>  {
+            return <h1 key={index}>{astro.name}</h1>
+        })
+
+        return (
+            <div>
+                {astronauts}
+            </div>
+        )
+    }
+}
+
+export default App
